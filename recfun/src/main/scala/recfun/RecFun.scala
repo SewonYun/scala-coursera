731f37bdd2c482ae83ff2f80c51cb8420878b322
윤세원 <later9459@cntech.pro>
2025-10-13 16:39:12 +0900

    fix: change monolith folder to each course folder

--- recfun/src/main/scala/recfun/RecFun.scala
+++ recfun/src/main/scala/recfun/RecFun.scala
@@ -8,18 +8,19 @@ object RecFun extends RecFunInterface:
   def main(args: Array[String]): Unit =
 
     // * Exercise 1
-    // println("Pascal's Triangle")
-    // for row <- 0 to 10 do
-    //   for col <- 0 to row do
-    //     print(s"${pascal(col, row)} ")
-    //   println()
+    println("Pascal's Triangle")
+    for row <- 0 to 10 do
+      for col <- 0 to row do
+        print(s"${pascal(col, row)} ")
+      println()
 
     // * Exercise 2
-    // val balanceResult: Boolean = balance("())(".toList)
-    // println(balanceResult)
+    val balanceResult: Boolean = balance(")((())()".toList)
+    println("balanceResult - -")
+    println(balanceResult)
 
     // * Exercise 3
-    val countConins: Int  = countChange(5, List(1, 3))
+    val countConins: Int  = countChange(5, List(1, 2, 3))
     println(countConins)
 
   /**
@@ -37,28 +38,32 @@ object RecFun extends RecFunInterface:
    * Exercise 2
    */
   def balance(chars: List[Char]): Boolean = {
+
+    if(chars.isEmpty) {
+      return true
+    }
     
-    def innerFun(first: Char, tails: List[Char], leftCount: Int, rightCount: Int): Boolean = {
+    def innerFun(head: Char, tails: List[Char], leftCount: Int, rightCount: Int): Boolean = {
       if(leftCount < rightCount) return false
 
       if(tails.isEmpty) {
         val lastRightCount: Int = {
-          if(first == ')') rightCount + 1
+          if(head == ')') rightCount + 1
           else rightCount
         }
-        if (first == '(' || leftCount != lastRightCount) return false
+        if (head == '(' || leftCount != lastRightCount) return false
         else return true
       }
 
-      if( first != '(' && first != ')') {
+      if( head != '(' && head != ')') {
         return innerFun(tails.head, tails.tail, leftCount, rightCount)
       }
 
-      if(first == '(') {
+      if(head == '(') {
         return innerFun(tails.head, tails.tail, leftCount + 1, rightCount)
       }
 
-      if(first == ')') {
+      if(head == ')') {
         return innerFun(tails.head, tails.tail, leftCount, rightCount + 1)
       }
       
